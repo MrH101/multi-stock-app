65fb67782c02ec38400c9a2c88cae3b760ff5081
Hoko Norest <noresthoko@gmail.com>
2022-11-24 00:10:07 +0200

    YES

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -60,5 +60,5 @@ class CheckoutForm(forms.Form):
     address = forms.CharField(max_length=255)
          
 class AddToCartForm(forms.Form):
-    quantity = forms.IntegerField(max_value=100)     
+    quantity = forms.IntegerField(max_value=10000)     
 
